c7d496f5259cbf22648c5c22d4525a511820869a
Mihaly Bak <nulloiinter84@yahoo.se>
2024-06-04 19:03:56 +0200

    Minimal sample

--- ST7789_Sample_working/ST7789_Sample_working.ino
+++ ST7789_Sample_working/ST7789_Sample_working.ino
@@ -22,62 +22,13 @@ void setup(void) {
   tft.setRotation(90);
   Serial.println(F("Initialized"));
 
-  uint16_t time = millis();
-  tft.fillScreen(ST77XX_BLACK);
-  time = millis() - time;
-
-  Serial.println(time, DEC);
-  delay(500);
-
-  // large block of text
-  tft.fillScreen(ST77XX_BLACK);
-  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST77XX_WHITE);
-  delay(1000);
-
   // tft print function!
   tftPrintTest();
   delay(4000);
-
-  // a single pixel
-  tft.drawPixel(tft.width()/2, tft.height()/2, ST77XX_GREEN);
-  delay(500);
-
-  // line draw test
-  testlines(ST77XX_YELLOW);
-  delay(500);
-
-  // optimized lines
-  testfastlines(ST77XX_RED, ST77XX_BLUE);
-  delay(500);
-
-  testdrawrects(ST77XX_GREEN);
-  delay(500);
-
-  testfillrects(ST77XX_YELLOW, ST77XX_MAGENTA);
-  delay(500);
-
-  tft.fillScreen(ST77XX_BLACK);
-  testfillcircles(10, ST77XX_BLUE);
-  testdrawcircles(10, ST77XX_WHITE);
-  delay(500);
-
-  testroundrects();
-  delay(500);
-
-  testtriangles();
-  delay(500);
-
-  mediabuttons();
-  delay(500);
-
-  Serial.println("done");
-  delay(1000);
 }
 
 void loop() {
-  tft.invertDisplay(true);
-  delay(500);
-  tft.invertDisplay(false);
+  delay(1000);
   delay(500);
 }
 
@@ -226,7 +177,7 @@ void tftPrintTest() {
   tft.setTextColor(ST77XX_BLUE);
   tft.setTextSize(4);
   tft.print(1234.567);
-  delay(1500);
+  delay(1500);/*
   tft.setCursor(0, 0);
   tft.fillScreen(ST77XX_BLACK);
   tft.setTextColor(ST77XX_WHITE);
@@ -246,7 +197,7 @@ void tftPrintTest() {
   tft.setTextColor(ST77XX_MAGENTA);
   tft.print(millis() / 1000);
   tft.setTextColor(ST77XX_WHITE);
-  tft.print(" seconds.");
+  tft.print(" seconds.");*/
 }
 
 void mediabuttons() {
